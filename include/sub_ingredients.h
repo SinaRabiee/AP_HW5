193b8014ff52d3eb88e45f0d5575dabf8c114b4f
root <root@DESKTOP-B3DIU7B.localdomain>
2022-05-20 11:18:06 +0430

    sub_ingredients implemented with multi-line Macro (Challenge section), added the build folder to gitignore

--- include/sub_ingredients.h
+++ include/sub_ingredients.h
@@ -1,4 +1,27 @@
 #ifndef SUB_INGREDIENTS_H
 #define SUB_INGREDIENTS_H
 
+#include <string>
+#include <vector>
+
+#define DEFCLASS(sub, amount)                                 \
+    class sub : public Ingredient {                           \
+    public:                                                   \
+        sub(size_t units)                                     \
+            : Ingredient { amount, units }                    \
+        {                                                     \
+            this->name = #sub;                                \
+        }                                                     \
+        virtual std::string get_name() { return this->name; } \
+    };
+
+DEFCLASS(Cinnamon, 5);
+DEFCLASS(Chocolate, 5);
+DEFCLASS(Sugar, 1);
+DEFCLASS(Cookie, 10);
+DEFCLASS(Espresso, 15);
+DEFCLASS(Milk, 10);
+DEFCLASS(MilkFoam, 5);
+DEFCLASS(Water, 1);
+
 #endif // SUB_INGREDIENTS_H
